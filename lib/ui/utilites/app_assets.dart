eecd03b08b328da9a6557e74e80692d0d4df136a
MoemenDarwish36 <moemendarwish36@gmail.com>
2024-10-30 01:40:36 -0700

    Add initial commit.

--- lib/ui/utilites/app_assets.dart
+++ lib/ui/utilites/app_assets.dart
@@ -2,4 +2,5 @@ abstract class AppAssets {
   static const String defaultPath = "assets/image/";
   static const String splashLight = "${defaultPath}splash_light.png";
   static const String background = "${defaultPath}main_background.png";
+/////
 }
