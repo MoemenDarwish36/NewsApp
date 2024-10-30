4252cf06daae1da69ec120fd5c8dad3e4a88ed7d
MoemenDarwish36 <moemendarwish36@gmail.com>
2024-10-30 01:57:24 -0700

    Add initial .

--- lib/ui/utilites/app_assets.dart
+++ lib/ui/utilites/app_assets.dart
@@ -2,5 +2,4 @@ abstract class AppAssets {
   static const String defaultPath = "assets/image/";
   static const String splashLight = "${defaultPath}splash_light.png";
   static const String background = "${defaultPath}main_background.png";
-/////
 }
